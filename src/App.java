a4dc042c63df3cce7deacd1cdd7eeea2eb7a2b22
DuDu Silva <raimundoeduardo.silva@gmail.com>
2025-03-05 14:41:29 -0300

    Vetor introdução

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import vetor.PrimeiroVetor;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       PrimeiroVetor vetor = new PrimeiroVetor(5);
+       vetor.adicionar(10);
+      vetor.exibir(); 
+
+    }
+}
